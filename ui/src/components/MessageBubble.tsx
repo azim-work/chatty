2c4f57f0b9c46873fc32756dd54e80830804cc4e
Azim Baghadiya <azim-work@outlook.com>
2025-08-18 21:46:17 -0400

    feat/ui: style message bubbles

--- ui/src/components/MessageBubble.tsx
+++ ui/src/components/MessageBubble.tsx
@@ -9,11 +9,13 @@ const MessageBubble = ({ role, content }: Props) => {
   const containerClass = "max-w-xl";
   const bubbleClass = isUser
     ? "bg-neutral-800 text-white px-4 py-2 rounded-b-xl rounded-tl-xl"
-    : "bg-gray-50 text-gray-800 text-sm px-4 py-2 rounded-b-xl rounded-tr-xl";
+    : "bg-gray-50 text-gray-800 text-lg px-4 py-2 rounded-b-xl rounded-tr-xl";
 
   return (
     <div
-      className={`flex flex-col ${isUser ? "items-end" : "items-start"} w-full`}
+      className={`flex flex-col ${
+        isUser ? "items-end" : "items-start"
+      } w-full mt-2`}
     >
       <div className={containerClass}>
         <div className={bubbleClass}>{content}</div>
